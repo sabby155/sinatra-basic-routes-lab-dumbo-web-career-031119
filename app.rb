65b85ef86037697c23d6d176f729a661dd9cbdf2
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-31 22:07:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    "Hello, World!"
+  end   
+  get '/name' do 
+    "My name is Sabrina"
+  end   
+  get '/hometown' do 
+    "My hometown is New York"
+  end 
+  get '/favorite-song' do 
+    "My favorite song is Midnight City"
+  end 
 end
